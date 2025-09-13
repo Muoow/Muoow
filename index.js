3ea41a58cd47f843abe7b82989291fc1559cde5a
ZhangXiang <144351162+Muoow@users.noreply.github.com>
2025-09-13 11:38:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+const thisYear = new Date().getFullYear()
+const startTimeOfThisYear = new Date(`${thisYear}-01-01T00:00:00+00:00`).getTime()
+const endTimeOfThisYear = new Date(`${thisYear}-12-31T23:59:59+00:00`).getTime()
+const progressOfThisYear = (Date.now() - startTimeOfThisYear) / (endTimeOfThisYear - startTimeOfThisYear)
+const progressBarOfThisYear = generateProgressBar()
 
+function generateProgressBar() {
+    const progressBarCapacity = 30
+    const passedProgressBarIndex = parseInt(progressOfThisYear * progressBarCapacity)
+    const progressBar =
+      '█'.repeat(passedProgressBarIndex) +
+      '▁'.repeat(progressBarCapacity - passedProgressBarIndex)
+    return `{ ${progressBar} }`
+}
+
+const readme = `
+<h1>Hi there, I'm Xiang Zhang! <img src="https://raw.githubusercontent.com/iampavangandhi/iampavangandhi/master/gifs/Hi.gif" width="30px"></h1>
+
+> [!TIP]
+> ⏳ Year Progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
+>
+> ⏰ Updated on ${new Date().toUTCString()}
+
+### About me
+* I am currently studying at the School of Computer Science and Technology, Tongji University
+
+### My GitHub
+<image src="https://github-readme-stats.vercel.app/api?username=Muoow&hide=contribs,prs" style="height: 180px"/>
+<image src="https://github-readme-stats.vercel.app/api/top-langs/?username=Muoow&layout=compact&theme=default" style="height: 180px"/>
+`
+
+console.log(readme)
